59b5b23d1ab22bfca78801ff77dd60292ed54b17
Kloping <3474006766@qq.com>
2023-12-12 13:28:34 +0800

    update

--- start.cmd
+++ start.cmd
@@ -1,3 +1,5 @@
+rmdir libs
+
 mvn clean dependency:copy-dependencies -DoutputDirectory=libs compile
 
 java -classpath ".\target\classes;.\libs\*" io.github.kloping.mihdp.MihDpMain -Dfile.encoding=UTF-8
